a21150f3d7cd0749f04404458aea297f3807a4f4
Aarón López <n_aaron5@hotmail.com>
2022-01-27 20:50:39 +0100

    iteration 4 done

--- js/index.js
+++ js/index.js
@@ -30,10 +30,18 @@ function calculateAll() {
 
 // ITERATION 4
 
+
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const target = event.currentTarget.parentNode.parentNode
+
+  target.parentNode.removeChild(target)
+
+  const total = document.querySelector("#total-value span")
+  const subtotal = target.querySelector(".subtotal span")
+
+  total.innerHTML = total.innerHTML - subtotal.innerHTML
+
 }
 
 // ITERATION 5
@@ -46,5 +54,8 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.querySelectorAll(".btn-remove");
+  removeButtons.forEach((eachButton) => {
+    eachButton.addEventListener("click", removeProduct);
+  })
 });
